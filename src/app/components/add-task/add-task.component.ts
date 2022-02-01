cc16f636d64a6f6a4477d5385f9ab98b112fa201
Keemchard <keemchardtamio@gmail.com>
2022-02-01 18:32:13 +0800

    modify logic for addtask submission form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -9,14 +9,23 @@ import { TaskService } from 'src/app/services/task.service';
 })
 export class AddTaskComponent implements OnInit {
   @Output() onAddTask: EventEmitter<taskInterface> = new EventEmitter();
-  task_title: string;
-  task_date: string;
+  task_title: string = '';
+  task_date: string = '';
   task_reminder: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
 
   onSubmit() {
+    let titleVar: HTMLInputElement = document.querySelector('#task_title');
+    let dayVar: HTMLInputElement = document.querySelector('#task_date');
+    let reminderVar: HTMLInputElement =
+      document.querySelector('#task_reminder');
+
+    if (titleVar.value == '' || dayVar.value == '' || reminderVar.value == '') {
+      alert('Please fill up all the input in that forms!');
+      return;
+    }
     const newTask = {
       task_title: this.task_title, // so dito dapat pareho ng name sa taskinterface or sa model
       task_date: this.task_date, //likedsa task interface, nalakagay is task_date, so dito dapat same name task date, as well as in other object
@@ -29,5 +38,7 @@ export class AddTaskComponent implements OnInit {
     this.task_title = '';
     this.task_date = '';
     this.task_reminder = false;
+
+    document.location.reload();
   }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'TaskTracker';
+  title: string = "KC's Task Tracker";
   constructor() {}
 
   ngOnInit(): void {}
